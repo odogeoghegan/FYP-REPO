5da599fb0d8aae05a656f305c22c3cc0747c54b8
Oisín Geoghegan <odogeoghegan@hotmail.com>
2023-01-25 17:50:12 +0000

    trying to resolve issue with images

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,35 @@
-import Image from 'next/image'
+import Image from "next/legacy/image";
 import React from 'react'
 
 function Header() {
   return (
     <div>
-        <div className='flex justify-between max-w-6xl'>
-            {/* Left */}
-            <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
-              <Image src="/Munchies-Logo.png" alt="logo" layout="fill" objectFit="contain"/>
-            </div>
-            <div className="relative h-10 w-10 lg:hidden flex-shrink-0 cursor-pointer">
-              <Image src="/Munchies-Icon.png" alt="logo" layout="fill" objectFit="contain"/>
-            </div>
-            {/* Middle */}
 
-            {/* Right */}
+      {/* Left */}
+      <div className='flex justify-between max-w-2xl'>
+        <div className="hidden lg:inline-grid h-24 w-24 cursor-pointer">
+          <Image 
+            src="/Munchies-Logo.png"
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
+
+        <div className=" w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image 
+            src="/Munchies-Icon.png" 
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+      {/* Middle */}
+      <div>
+        <input type="text" placeholder="search" />
+      </div>
+
+      {/* Right */}
+      </div>
     </div>
   )
 }
