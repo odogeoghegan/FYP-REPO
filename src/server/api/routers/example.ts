90da813dbc982e1e22514d96650bbade03ca423b
Oisín Geoghegan <odogeoghegan@hotmail.com>
2023-02-01 15:15:40 +0000

    new migration files & and new db test deploy

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -11,7 +11,8 @@ export const exampleRouter = createTRPCRouter({
       };
     }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
+  getAll: publicProcedure
+  .query(({ ctx }) => {
     return ctx.prisma.example.findMany();
   }),
 
