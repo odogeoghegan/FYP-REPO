854f553c36468d3a83deb79a9c71ab50597ea20a
Oisín Geoghegan <odogeoghegan@hotmail.com>
2023-02-26 04:34:34 +0000

    eslint disbaled rule empty promise

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -8,14 +8,13 @@ type SignInProps = {
 function SignIn({ providers }: SignInProps) {
   return (
     <>
-
       <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-32 px-14 text-center">
         <img className=" w-96 rounded-3xl" alt="logo" src="/Munchies-Brand-Logo.png" />
         <p className='pt-4'>WHOOOOO custom sign in page</p>
         <div className='mt-8'>
           {Object.values(providers).map((provider) => (
             <div key={provider.name}>
-              <button className="p-3 bg-orange-500 rounded-lg text-black font-bold" onClick={() => SignIntoProvider(provider.id, { callbackUrl: "/" ?? ""})}>
+              <button className="p-3 bg-orange-500 rounded-lg text-black font-bold" onClick={() => SignIntoProvider(provider.id, { callbackUrl: "/" })}>
                 Sign in with {provider.name}
               </button>
             </div>
