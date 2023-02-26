a3d6dec88fabbd34583d1446a49580bf76e76a74
Oisín Geoghegan <odogeoghegan@hotmail.com>
2023-02-26 13:44:42 +0000

    Added next auth secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -34,6 +34,7 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/auth/signin",
   },
