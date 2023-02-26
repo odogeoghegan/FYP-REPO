4874ba39aadc8a40bf231c468f18366cca35f736
Oisín Geoghegan <odogeoghegan@hotmail.com>
2023-02-26 04:58:52 +0000

    custom sign in empty promise errors

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,9 +1,9 @@
-import { getProviders, signIn as SignIntoProvider } from 'next-auth/react'
+import { getProviders, useSession, signIn as SignIntoProvider } from 'next-auth/react'
 import React from 'react'
 
 type SignInProps = {
   providers: Record<string, { id: string; name: string }>
-}
+};
 
 function SignIn({ providers }: SignInProps) {
   return (
@@ -14,7 +14,7 @@ function SignIn({ providers }: SignInProps) {
         <div className='mt-8'>
           {Object.values(providers).map((provider) => (
             <div key={provider.name}>
-              <button className="p-3 bg-orange-500 rounded-lg text-black font-bold" onClick={() => SignIntoProvider(provider.id, { callbackUrl: "/" })}>
+              <button className="p-3 bg-orange-500 rounded-lg text-black font-bold" onClick={async () => {await SignIntoProvider(provider.id, { callbackUrl: "/" })}}>
                 Sign in with {provider.name}
               </button>
             </div>
@@ -24,6 +24,7 @@ function SignIn({ providers }: SignInProps) {
     </>
   )
 }
+
 
 export async function getServerSideProps() {
   const providers = await getProviders();
