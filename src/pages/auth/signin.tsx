1c32eae6ddafb6f082152f2529b898f1f0c44c60
Oisín Geoghegan <odogeoghegan@hotmail.com>
2023-02-26 14:14:23 +0000

    Type error signin page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,11 +1,11 @@
-import { getProviders, signIn as SignIntoProvider } from 'next-auth/react'
+import { getProviders, useSession, signIn as SignIntoProvider } from 'next-auth/react'
 import React from 'react'
 
 type SignInProps = {
   providers: Record<string, { id: string; name: string }>
 };
 
-function SignIn({ providers }: SignInProps) {
+function signIn({ providers }: SignInProps) {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-32 px-14 text-center">
@@ -14,7 +14,7 @@ function SignIn({ providers }: SignInProps) {
         <div className='mt-8'>
           {Object.values(providers).map((provider) => (
             <div key={provider.name}>
-              <button className="p-3 bg-orange-500 rounded-lg text-black font-bold" onClick={() => void SignIntoProvider(provider.id, { callbackUrl: "/" })}>
+              <button className="p-3 bg-orange-500 rounded-lg text-black font-bold" onClick={() => {SignIntoProvider(provider.id, { callbackUrl: "/" })}}>
                 Sign in with {provider.name}
               </button>
             </div>
@@ -24,7 +24,6 @@ function SignIn({ providers }: SignInProps) {
     </>
   )
 }
-
 
 export async function getServerSideProps() {
   const providers = await getProviders();
@@ -36,4 +35,4 @@ export async function getServerSideProps() {
   }
 }
 
-export default SignIn
+export default signIn
