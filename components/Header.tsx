bd0c3bc77b18d585b94000a42a353678fbe9830e
Oisín Geoghegan <odogeoghegan@hotmail.com>
2023-04-04 21:42:34 +0100

    small screen menu button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import _app from "../src/pages";
 import { useRecoilState } from "recoil";
 import { createPostModalAtom } from "../atoms/createPostModalAtom";
@@ -17,7 +17,20 @@ import Link from "next/link";
 function Header() {
   const [openCreatePost, setOpenCreatePost] = useRecoilState(createPostModalAtom);
   const [openRecipeGenerator, setOpenRecipeGenerator] = useRecoilState(recipeGeneratorModalAtom);
+  const [smallHeader, setSmallHeader] = useState(false);
   const { data: session, status } = useSession();
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) { // set the breakpoint here
+        setSmallHeader(false); // reset the state if the screen size goes above the breakpoint
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
 
   return (
@@ -52,19 +65,57 @@ function Header() {
 
           {/* Middle */}
           <div className="max-w-xs">
-            <div className="relative mt-1 p-3 rounded-md">
-              <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-                <BiSearch className="h-5 w-5 text-gray-500" />
+            {smallHeader ? null : (
+              <div className="relative mt-1 p-3 rounded-md">
+                <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                  <BiSearch className="h-5 w-5 text-gray-500" />
+                </div>
+                <input
+                  className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black"
+                  type="text"
+                  placeholder="search"
+                />
               </div>
-              <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black"
-                type="text"
-                placeholder="search" />
-            </div>
+            )}
           </div>
 
           {/* Right */}
           <div className="flex items-center justify-end space-x-4">
-            <BiMenu className="h-10 md:hidden cursor-pointer hover:scale-150 transition-all duration-150 ease-out" size="30"  />
+              {smallHeader ? (
+                <>
+                  <Link href="/">
+                    <BiHome
+                      className="navBtnSH"
+                      aria-label="home page button"
+                      aria-describedby="default home page with post feed"
+                      size="30"
+                    />
+                  </Link>
+                  {session ? (
+                    <>
+                      <BiBookHeart
+                        onClick={() => setOpenRecipeGenerator(true)}
+                        className="navBtnSH"
+                        size="30"
+                      />
+                      <BiPlusCircle
+                        onClick={() => setOpenCreatePost(true)}
+                        className="navBtnSH"
+                        size="30"
+                      />
+                      <BiGroup className="navBtnSH" size="30" />
+                      <BiHeart className="navBtnSH" size="30" />
+                    </>
+                  ) : (
+                    null
+                  )}
+                </>
+              ) : null}
+            <BiMenu
+              className="h-10 md:hidden cursor-pointer hover:scale-150 transition-all duration-150 ease-out"
+              size="30"
+              onClick={() => setSmallHeader((prevState) => !prevState)}
+            />
             <Link href="/">
               <BiHome className="navBtn" aria-label="home page button" aria-describedby="default home page with post feed" size="30" />
             </Link>
@@ -79,6 +130,7 @@ function Header() {
             ) : (
               <button onClick={() => void signIn()}>Sign In</button>
             )}
+
           </div>
         </div>
       </div>
